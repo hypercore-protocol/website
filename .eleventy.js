975d85d975b42aab686a0c7369929fd04cc1ef74
Paul Frazee <pfrazee@gmail.com>
2020-12-15 13:53:13 -0600

    Add 11ty site generator

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = (eleventyConfig) => {
+  // copy the following items to _site
+  eleventyConfig.addPassthroughCopy('favicon.ico')
+  eleventyConfig.addPassthroughCopy('images')
+  eleventyConfig.addPassthroughCopy('videos')
+  eleventyConfig.addPassthroughCopy('fonts')
+}
---
+++ _includes/layouts/base.11ty.js
@@ -0,0 +1,35 @@
+exports.data = {
+  title: 'Hypercore Protocol'
+}
+
+exports.render = function (data) {
+  return `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>${data.title}</title>
+
+    <!-- SEO -->
+    <meta name="Description" content="A fast, scalable, and secure peer-to-peer protocol for everyone.">
+    <meta name="keywords" content="decentralization,hypercore,peer-to-peer,p2p,hyperdrive">
+
+    <!-- Social -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@HypercoreProto">
+    <meta name="twitter:creator" content="@HypercoreProto">
+    <meta name="og:title" content="Hypercore Protocol">
+    <meta name="og:description" content="A fast, scalable, and secure peer-to-peer protocol for everyone.">
+    <meta name="twitter:image" content="https://hypercore-protocol.org/images/hypercore-protocol.png">
+
+    <link rel="stylesheet" type="text/css" href="/fonts/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    ${data.content}
+    <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""></noscript>
+  </body>
+</html>
+`
+}
