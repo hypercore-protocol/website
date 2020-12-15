2375df48bcb9c440bf8e3c81d97911447b2d457f
Paul Frazee <pfrazee@gmail.com>
2020-12-15 15:56:03 -0600

    Add sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginSass = require("eleventy-plugin-sass")
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(pluginSass)
+
   // copy the following items to _site
   eleventyConfig.setTemplateFormats(['md', 'html', 'css', 'ejs'])
   eleventyConfig.addPassthroughCopy('favicon.ico')
